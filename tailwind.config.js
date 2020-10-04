c9d703839f59b51b9337e865d4c60530355b621d
s1dequest <maxzintel@gmail.com>
2020-10-04 19:24:46 -0400

    Add hover, focus, and active styles to button.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,8 @@ module.exports = {
   theme: {
     extend: {},
   },
-  variants: {},
+  variants: {
+    backgroundColor: ['responsive','hover','focus','active'] // order important here for cascade.
+  }, // hover, focus, active, etc... are variants
   plugins: [],
 }
